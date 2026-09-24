dbf63988fddf72b8d64dd6c42426f093ee9ae978
Valentas <333529609+valentasmockuss-hash@users.noreply.github.com>
2026-09-25 01:28:48 +0300

    Pridetas vidurkio medianos ir galutinio pazymo skaiciavimas

--- v0.1_projektas.cpp
+++ v0.1_projektas.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,9 +12,44 @@ struct Studentas {
     vector<int> nd;
     int egzaminas=0;
 };
+
+double vidurkis (const vector<int>& nd){
+    if (nd.empty()) return 0.0;
+    double suma=0.0;
+    for (int pazymys : nd){
+        suma +=pazymys;
+    }
+    return suma/nd.size();
+}
+
+double mediana(vector<int> nd){
+    if (nd.empty()) return 0.0;
+    sort(nd.begin(), nd.end());
+    size_t n=nd.size();
+
+    if (n%2==1){
+        return nd[n/2];
+    }
+    return (nd[n/2-1]+nd[n/2])/2.0;
+}
+double galutinis(double ndRezultatas, int egzaminas){
+    return 0.4*ndRezultatas + 0.6*egzaminas;
+}
+
 int main(){
-    vector<Studentas> studentai;
-    cout<<"Studentu pazymiu programa\n";
-    cout<<"Studentu skaicius:"<<studentai.size()<<"\n";
+    Studentas s;
+    s.vardas="Jonas";
+    s.pavarde="Jonaitis";
+    s.nd={10, 4, 9, 8};
+    s.egzaminas=8;
+
+    double ndVidurkis=vidurkis(s.nd);
+    double ndMediana=mediana(s.nd);
+
+    cout<<fixed<<setprecision(2);
+    cout<<s.vardas<<" "<<s.pavarde<<"\n";
+    cout<<"Galutinis (vidurkis):"<<galutinis(ndVidurkis, s.egzaminas)<<"\n";
+    cout<<"Galutinis (mediana):"<<galutinis(ndMediana, s.egzaminas)<<"\n";
     return 0;
 }
+
